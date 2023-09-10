54e0e543d2568dfc0d16138063fe7395fe5eaf27
Chinonso Wisdom <nonsowisdom62@gmail.com>
2023-09-10 20:34:54 +0100

    database host

--- Diskord/settings.py
+++ Diskord/settings.py
@@ -100,7 +100,7 @@ DATABASES = {
         "NAME": os.environ.get("DATABASE_NAME", ""),
         "USER": os.environ.get("DATABASE_USER", ""),
         "PASSWORD": os.environ.get("DATABASE_PASSWORD", ""),
-        "HOST": os.environ.get("DATABASE_HOST", ""),
+        "HOST": os.environ.get("CLEARDB_DATABASE_URL"),
         "PORT": "3306",
     }
 }
